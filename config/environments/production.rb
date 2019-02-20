4a5ad3ae3fd55b748ee39230e231bec1a5d857f7
RampEX <sebastian@rampex.com>
2019-02-20 09:16:16 -0700

    redis to go url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store
+  config.cache_store = :redis_cache_store, {url: "redis://redistogo:382a2d6db1a02ca067e58db5354d9efc@tarpon.redistogo.com:11611/"}
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
